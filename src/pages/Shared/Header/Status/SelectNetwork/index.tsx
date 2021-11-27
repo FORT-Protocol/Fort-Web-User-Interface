273c951ca9238337769fc8563d3b83e77bac9159
MLY0813 <92143869+ivecoGolden@users.noreply.github.com>
2021-11-27 17:56:12 +0800

    update: change network

---
+++ src/pages/Shared/Header/Status/SelectNetwork/index.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react'
+import { LittleBSC, LittleETH, NetworkNow } from '../../../../../components/Icon'
+import MainCard from '../../../../../components/MainCard'
+import './styles'
+
+const SelectNetwork: FC = () => {
+    const classPrefix = 'selectNetwork'
+    return (
+        <div className={classPrefix}>
+            <div className={`${classPrefix}-chainName`}>
+                <LittleBSC/><p>Ethereum</p>
+            </div>
+            <div className={`${classPrefix}-hover`}>
+                <MainCard classNames={`${classPrefix}-ul`}>
+                    <p>Select a network</p>
+                    <ul>
+                        <li><a href={'https://app.hedge.red'}><LittleETH/><p>Ethereum</p><NetworkNow/></a></li>
+                        <li><a href={'https://test.hedge.red'}><LittleETH/><p>Rinkeby</p></a></li>
+                        <li><a href={'https://bsc.hedge.red'}><LittleBSC/><p>BSC</p></a></li>
+                    </ul>
+                </MainCard>
+            </div>
+            
+        </div>
+    )
+}
+
+export default SelectNetwork
