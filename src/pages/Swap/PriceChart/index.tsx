5093c6540835326d9cba74289c458920cdd708df
tunogya <tunogya@qq.com>
2022-03-30 20:09:50 +0800

    fixed bug

--- src/pages/Swap/PriceChart/index.tsx
+++ src/pages/Swap/PriceChart/index.tsx
@@ -1,16 +1,12 @@
 import { Area } from '@ant-design/plots';
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import {useDate} from "../../../libs/hooks/useDate";
 
 const PriceChart = () => {
   const [data, setData] = useState([]);
   const { today, aMonthAgo } = useDate()
 
-  useEffect(() => {
-    asyncFetch();
-  }, []);
-
-  const asyncFetch = () => {
+  const asyncFetch = useCallback(() => {
     const JWT_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxIiwic3ViIjoiYWRtaW4xMjMiLCJpYXQiOjE2NDMxNzY0NDksImlzcyI6ImFkbWluMTIzIiwiZXhwIjoxOTU4NTM2NDQ5LCJsZXZlbCI6ImFkbWluIiwicm9sZXMiOltdLCJtb2JpbGUiOiIxNTg5NzgzMDQxNCIsInVzZXJpZCI6IjEiLCJ1c2VybmFtZSI6ImFkbWluMTIzIn0.U9Obn_JybKpczWJ2VibAxNX-mHoMmP3eQPElNIf2QLM'
 
     fetch(`https://work.parasset.top/workbench-api/futures/DCUPrice?from=${aMonthAgo}&to=${today}`, {
@@ -25,7 +21,11 @@ const PriceChart = () => {
       .catch((error) => {
         console.log('fetch data failed', error);
       });
-  };
+  }, [aMonthAgo, today])
+
+  useEffect(() => {
+    asyncFetch();
+  }, [asyncFetch]);
 
   const config = {
     data,
