d7bdae68253201ab5802dc562e63953a6ab78de8
MLY0813 <674740416@qq.com>
2022-04-21 15:18:31 +0800

    update

--- src/pages/Win/WinOrderList/index.tsx
+++ src/pages/Win/WinOrderList/index.tsx
@@ -19,14 +19,16 @@ const WinOrderList: FC<WinOrderListProps> = ({ ...props }) => {
   const [isHistory, setIsHistory] = useState<Boolean>(false);
 
   const historyLi = props.historyList.map((item) => {
-    const itemAmount = BigNumber.from(item.m.toString()).div(BigNumber.from('10000'))
+    // const itemAmount = BigNumber.from(item.m.toString()).div(BigNumber.from('10000'))
+    const showNum = 100 / (parseFloat(item.m.toString()) / 10000)
     return (
       <li key={item.owner + item.index.toString()}>
         <HistoryTime
           blockNum={BigNumber.from(item.openBlock.toString()).toNumber()}
         />
         <p className={`${classPrefix}-historyList-right`}>
-          {BigNumber.from("0").eq(item.n) ? itemAmount.toString() : 0} DCU
+          {/* {BigNumber.from("0").eq(item.n) ? itemAmount.toString() : 0} DCU */}
+          {`${showNum} %`}
         </p>
       </li>
     );
